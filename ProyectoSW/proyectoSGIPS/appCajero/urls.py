4303d667b927c77e8a7ade544eb86791fd1a159d
mattichief117 <matias.osorio.q@mail.pucv.cl>
2022-07-01 17:15:00 -0400

    cajero Listo

--- ProyectoSW/proyectoSGIPS/appCajero/urls.py
+++ ProyectoSW/proyectoSGIPS/appCajero/urls.py
@@ -6,7 +6,7 @@ from appCajero import views
 urlpatterns = [
 
     path('', views.cajero_general, name="cajero"),
-    path('cajero_registrar_venta/', views.cajero_registrar_venta, name="login"),
-    path('cajero_registrar_turno/', views.cajero_registrar_turno, name="login"),
+    path('cajero_registrar_venta/', views.cajero_registrar_venta, name="RegistrarVenta"),
+    path('cajero_registrar_turno/', views.cajero_registrar_turno, name="RegistrarTurno"),
 
 ]
